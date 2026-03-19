88db93f4c63369645d35092b0bc31f3828229039
kera9960 <kera9960@gmail.com>
2026-03-19 20:23:14 +0900

    feat: 입력 검증 기능 추가

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -3,16 +3,48 @@ package com.example.calculator;
 import java.util.Scanner;
 
 public class App {
-    public static void main() {
+    static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         String output;
 
         do {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int firstNum = sc.nextInt();
+            int firstNum;
+            while (true){                                           // 검증 반복문
+                System.out.print("첫 번째 숫자를 입력하세요: ");
 
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int secondNum = sc.nextInt();
+                if (sc.hasNextInt()){                               // 정수인지 검증 boolean
+                    firstNum = sc.nextInt();
+
+                    if (firstNum >= 0 ){                            // 양수인지 검증
+                        break;
+                    } else {
+                        System.out.println("양수를 입력하세요");
+                    }
+                } else {
+                    System.out.println("숫자를 입력하세요");
+                    sc.next();                                      // sc 입력 버퍼에 남은거 소비하는 기능
+                }
+            }
+
+            int secondNum;
+
+            while (true){
+                System.out.print("두 번째 숫자를 입력하세요: ");
+
+                if (sc.hasNextInt()){
+                    secondNum = sc.nextInt();
+
+                    if (secondNum >= 0 ){
+                        break;
+                    } else {
+                        System.out.println("양수를 입력하세요");
+                    }
+                } else {
+                    System.out.println("숫자를 입력하세요");
+                    sc.next();
+                }
+            }
 
             System.out.println("사칙연산 기호를 입력하세요 ( + , - , * , / ):  ");
             String operators = sc.next();
