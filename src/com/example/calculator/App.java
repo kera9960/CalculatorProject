64448c7b17d43c4818b9d9d723da7e72d98d1475
kera9960 <kera9960@gmail.com>
2026-03-20 20:27:39 +0900

    feat: Calculator 클래스에 계산 및 결과 저장 기능 추가

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -3,10 +3,11 @@ package com.example.calculator;
 import java.util.Scanner;
 
 public class App {
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
+        Calculator cal = new Calculator();                          // Calculator 클래스 객체 만들기
         Scanner sc = new Scanner(System.in);
-        String output;
+        String output;                                              // 전역변수 클래스변수
 
         do {
             int firstNum;
@@ -28,7 +29,6 @@ public class App {
             }
 
             int secondNum;
-
             while (true){
                 System.out.print("두 번째 숫자를 입력하세요: ");
 
@@ -46,44 +46,25 @@ public class App {
                 }
             }
 
-            System.out.println("사칙연산 기호를 입력하세요 ( + , - , * , / ):  ");
+            System.out.print("사칙연산 기호를 입력하세요 ( + , - , * , / ):  ");
             String operators = sc.next();
             char operator = operators.charAt(0);
 
-            switch (operator){
+            try {
+                int result = cal.calculate(firstNum,secondNum,operator);
+                System.out.println("결과: " +result );
 
-                case '+' :
-                    int plusNum = firstNum + secondNum;
-                    System.out.println("결과: " + plusNum);
-                    break;
-
-                case '-' :
-                    int minusNum = firstNum - secondNum;
-                    System.out.println("결과: " + minusNum);
-                    break;
-
-                case '*' :
-                    int multiplyNum = firstNum * secondNum;
-                    System.out.println("결과: " + multiplyNum);
-                    break;
-
-                case '/' :
-                    if (secondNum == 0){
-                    System.out.println("0으로 나눌 수 없습니다.");
-                } else {
-                    int divideNum = firstNum / secondNum;
-                    System.out.println("결과: " + divideNum);
-                }
-                break;
-
-                default:
-                    System.out.println("잘못된 연산자입니다");
-
+            } catch (ArithmeticException e){
+                System.out.println("0으로 나눌 수 없습니다.");
+            } catch (IllegalArgumentException e){
+                System.out.println("잘못된 연산자입니다.");
             }
 
             System.out.print("종료하려면 exit를 입력하세요. 계속하려면 아무 값이나 입력하세요.: ");
             output = sc.next();
 
         } while (!output.equals("exit"));
+
+        sc.close();
     }
 }
--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,7 +1,38 @@
 package com.example.calculator;
 
-public class Calculator {
-    static void main() {
+import java.util.ArrayList;
 
+public class Calculator {
+
+    ArrayList<Integer> resultList = new ArrayList<Integer>();
+
+    int calculate(int firstNum, int secondNum, char operator){
+            int result;
+
+            switch (operator){
+
+                case '+': result = firstNum + secondNum;
+                break;
+
+                case '-': result = firstNum - secondNum;
+                break;
+
+                case '*': result = firstNum * secondNum;
+                break;
+
+                case '/': if (secondNum==0){
+                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                } else {
+                    result = firstNum / secondNum;
+                    break;
+                }
+                default:
+                    throw new IllegalArgumentException("지원하지 않는 연산자입니다");
+            }
+
+            resultList.add(result);
+
+            return result;
     }
+
 }
